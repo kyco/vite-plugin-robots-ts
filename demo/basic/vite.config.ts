de17cfa2f71a6c69e3da0a32445b0552f2007344
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-03-19 17:31:14 +0100

    fix(demo): Use constant for content

--- demo/basic/vite.config.ts
+++ demo/basic/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 
-import { robots } from '../../src/index.ts'
+import { BLOCK_AI_ALLOW_REST, robots } from '../../src/index.ts'
 
 export default defineConfig({
   plugins: [
     robots({
-      block: 'ai-training',
+      content: BLOCK_AI_ALLOW_REST,
       sitemap: 'https://example.com/sitemap.xml',
     }),
   ],
